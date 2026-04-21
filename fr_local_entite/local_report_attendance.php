12f711365a6f26fd22a7c88bb4b419972d37d5af
ericfalcon <42006279+ericfalcon@users.noreply.github.com>
2026-04-21 08:39:04 +0000

    FR entite: add missing strings for report_attendance

--- fr_local_entite/local_report_attendance.php
+++ fr_local_entite/local_report_attendance.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Local language pack from https://learnpre.norm-uni.fr
+ * Strings for component 'local_report_attendance', language 'fr'
  *
  * @package    local
  * @subpackage report_attendance
@@ -27,44 +26,44 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['attendance'] = 'Participants';
 $string['certificate'] = 'Certificat';
-$string['companies'] = 'Sociétés';
-$string['completed'] = 'Complété';
-$string['completedusers'] = 'Complété';
-$string['completiondate'] = 'Date d\'achèvement';
-$string['completionenabled'] = 'Achèvement activé';
+$string['companies'] = 'Entités';
+$string['completed'] = 'Terminé';
+$string['completedusers'] = 'Utilisateurs ayant terminé';
+$string['completiondate'] = 'Date d’achèvement';
+$string['completionenabled'] = 'Suivi de l’achèvement activé';
 $string['completionisenabled'] = 'Oui';
-$string['completionnotenabled'] = 'Le suivi de l\'achèvement n\'est pas activé pour ce cours (aucune donnée de rapport disponible)';
+$string['completionnotenabled'] = 'Le suivi de l’achèvement n’est pas activé pour ce cours (aucune donnée de rapport disponible).';
 $string['course'] = 'Cours';
 $string['coursename'] = 'Nom du cours';
 $string['coursesummary'] = 'Aperçu du cours';
-$string['courseusers'] = 'Résumé de l\'utilisateur pour le cours -';
-$string['datecompleted'] = 'Date d\'achèvement';
-$string['datestarted'] = 'Date d\'attribution/début';
-$string['downloadcsv'] = 'Télécharger en tant que fichier CSV';
-$string['error'] = 'Erreur lors de la recherche du champ de profil personnalisé pour l\'école';
-$string['event'] = 'Événement de formation -';
+$string['courseusers'] = 'Résumé des utilisateurs pour le cours';
+$string['datecompleted'] = 'Date d’achèvement';
+$string['datestarted'] = 'Date de début';
+$string['downloadcsv'] = 'Télécharger au format CSV';
+$string['error'] = 'Erreur lors de la recherche du champ de profil personnalisé pour l’entité.';
+$string['event'] = 'Événement de formation';
 $string['finalscore'] = 'Score final';
 $string['inprogress'] = 'En cours';
-$string['inprogressusers'] = 'Encore en progrès';
-$string['invalidcourse'] = 'L\'ID de cours réussi n\'est pas valide pour cette entreprise/département';
-$string['iomad_attendance:view'] = 'rapport de présence iomad';
+$string['inprogressusers'] = 'En cours de progression';
+$string['invalidcourse'] = 'L’identifiant du cours n’est pas valide pour cette entité ou ce département.';
+$string['iomad_attendance:view'] = 'Voir le rapport de présence IOMAD';
 $string['name'] = 'Nom';
-$string['nocourses'] = '<h2>Il n\'y a aucun événement de formation</h2> <p>Aucune activité d\'événement de formation n\'est configurée dans aucun de vos cours. </p>';
-$string['nodata'] = 'Aucune donnée de rapport';
-$string['notstarted'] = 'Pas commencé';
-$string['notstartedusers'] = 'Pas commencé';
+$string['nocourses'] = '<h2>Aucun événement de formation</h2><p>Aucune activité d’événement de formation n’est configurée dans vos cours.</p>';
+$string['nodata'] = 'Aucune donnée disponible pour ce rapport';
+$string['notstarted'] = 'Non commencé';
+$string['notstartedusers'] = 'Non commencé';
 $string['nousers'] = 'Aucun utilisateur dans ce cours';
-$string['numusers'] = 'nombre d\'utilisateurs';
+$string['numusers'] = 'Nombre d’utilisateurs';
 $string['participant'] = 'Participant';
-$string['percent'] = '% complété';
+$string['percent'] = '%'; // Gardé volontairement tel quel si utilisé comme symbole
 $string['percentage'] = 'Pourcentage achevé';
 $string['pluginname'] = 'Rapport de présence par cours';
-$string['privacy:metadata'] = 'Le rapport de présence Iomad local affiche uniquement les données stockées dans d\'autres emplacements.';
+$string['privacy:metadata'] = 'Le rapport de présence IOMAD affiche uniquement des données stockées dans d’autres composants.';
 $string['remaining'] = 'Restant';
 $string['repcourseattendance'] = 'Rapport de présence par cours';
-$string['reportselect'] = 'Filtre de rapport';
-$string['report_attendance:view'] = 'Voir le rapport de présence au cours iomad';
-$string['select'] = '--sélectionner--';
-$string['status'] = 'Statuts';
-$string['trackedcount'] = 'Nombre de ressources/activités suivies :';
-$string['userrecordnotfound'] = 'L\'enregistrement de l\'utilisateur est introuvable';
+$string['reportselect'] = 'Filtre du rapport';
+$string['report_attendance:view'] = 'Consulter le rapport de présence des cours IOMAD';
+$string['select'] = '-- Sélectionner --';
+$string['status'] = 'Statut';
+$string['trackedcount'] = 'Nombre d’activités suivies :';
+$string['userrecordnotfound'] = 'L’enregistrement de l’utilisateur est introuvable.';
